1152a91eeb41bffbc4a0d523ed8bf93a941cf353
henhimicons <henhimicons@qq.com>
2021-10-02 23:02:57 +0800

    initial repository

---
+++ 1002/main.cpp
@@ -0,0 +1,16 @@
+#include <cstdlib>
+#include <cstdio>
+#include <cstring>
+const int maxn = 5005;
+char number[maxn];
+char substr[57][maxn];
+int main()
+{
+    scanf("%s",number);
+    int n;
+    scanf("%d",&n);
+    for (int i=0;i<n;i++)
+        scanf("%s",substr[i]);
+    
+    return 0;
+}
